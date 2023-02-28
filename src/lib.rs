6c908a7df32caf059f33909808aec2117ba56ddb
Nick Gal <nickgal@me.com>
2023-02-28 13:30:27 -0800

    Add cab export and rename

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,165 @@
-use std::fs::File;
+use std::{
+    borrow::Cow,
+    collections::HashMap,
+    fs::{self, File},
+    io::{self, Read, Seek},
+    path::{Path, PathBuf},
+};
 
-use binrw::{io::BufReader, BinRead, BinResult};
+use binrw::BinRead;
+use cab::{Cabinet, CompressionType, FileEntry};
+use filetime::FileTime;
 use msce_000::MSCE000;
+use once_cell::sync::Lazy;
+use time::OffsetDateTime;
 
 pub mod msce_000;
 pub mod strings;
 
-pub fn read_msce000(name: &str) -> BinResult<MSCE000> {
-    let mut reader = read_file(name);
-    MSCE000::read(&mut reader)
+static CE_DIRS: Lazy<HashMap<&str, &str>> = Lazy::new(|| {
+    let mut m = HashMap::new();
+    m.insert("%CE1%", r"Program Files");
+    m.insert("%CE2%", r"Windows");
+    m.insert("%CE3%", r"Windows\Desktop");
+    m.insert("%CE4%", r"Windows\StartUp");
+    m.insert("%CE5%", r"My Documents");
+    m.insert("%CE6%", r"Program Files\Accessories");
+    m.insert("%CE7%", r"Program Files\Communications");
+    m.insert("%CE8%", r"Program Files\Games");
+    m.insert("%CE9%", r"Program Files\Pocket Outlook");
+    m.insert("%CE10%", r"Program Files\Office");
+    m.insert("%CE11%", r"Windows\Programs");
+    m.insert("%CE12%", r"Windows\Programs\Accessories");
+    m.insert("%CE13%", r"Windows\Programs\Communications");
+    m.insert("%CE14%", r"Windows\Programs\Games");
+    m.insert("%CE15%", r"Windows\Fonts");
+    m.insert("%CE16%", r"Windows\Recent");
+    m.insert("%CE17%", r"Windows\Favorites");
+    m
+});
+
+pub fn read_cab(name: &str) -> Cabinet<File> {
+    let cab_file = File::open(name).unwrap();
+    Cabinet::new(cab_file).unwrap()
 }
 
-fn read_file(name: &str) -> BufReader<File> {
-    let f = File::open(name).expect("Failed to open");
-    BufReader::new(f)
+pub trait CECabinet {
+    fn find_000_manifest(&self) -> Option<&FileEntry>;
+    fn read_000_manifest(&self) -> MSCE000;
+}
+
+impl<R: Read + Seek> CECabinet for Cabinet<R> {
+    fn find_000_manifest(&self) -> Option<&FileEntry> {
+        for folder in self.folder_entries() {
+            for file in folder.file_entries() {
+                if file.extension() == "000" {
+                    return Some(file);
+                }
+            }
+        }
+        None
+    }
+
+    fn read_000_manifest(&self) -> MSCE000 {
+        let manifest = self
+            .find_000_manifest()
+            .expect("Failed to locate .000 manifest.");
+        let mut file = File::open(manifest.name()).unwrap();
+        MSCE000::read(&mut file).unwrap()
+    }
+}
+
+trait FileEntryExtension {
+    fn extension(&self) -> String;
+}
+
+impl FileEntryExtension for FileEntry {
+    fn extension(&self) -> String {
+        Path::new(self.name())
+            .extension()
+            .unwrap()
+            .to_string_lossy()
+            .to_string()
+    }
+}
+
+pub fn list_files<R: Read + Seek>(
+    cabinet: &mut Cabinet<R>,
+    msce: &MSCE000,
+) -> Vec<WinCECabFileEntry> {
+    let mut entries = Vec::new();
+
+    for folder in cabinet.folder_entries() {
+        for file in folder.file_entries() {
+            let file_ext = file.extension();
+            let file_path = match msce.file_mapping.get(&file_ext) {
+                Some(path) => path,
+                _ => match file_ext.as_ref() {
+                    "000" => "manifest.bin",
+                    "999" => "setup.dll",
+                    _ => file.name(),
+                },
+            };
+
+            let odt = match file.datetime() {
+                Some(pdt) => pdt.assume_utc(),
+                _ => OffsetDateTime::now_utc(),
+            };
+            let date_time = FileTime::from_unix_time(odt.unix_timestamp(), 0);
+
+            entries.push(WinCECabFileEntry {
+                cab_filename: file.name().to_string(),
+                compression: folder.compression_type(),
+                file_time: Some(date_time),
+                file_size: file.uncompressed_size(),
+                destination: file_path.to_string(),
+            });
+        }
+    }
+
+    entries
+}
+
+pub fn export_files<R: Read + Seek>(
+    cabinet: &mut Cabinet<R>,
+    file_entries: &Vec<WinCECabFileEntry>,
+) {
+    for file_entry in file_entries {
+        let mut reader = cabinet.read_file(&file_entry.cab_filename).unwrap();
+        let mut output_filepath = PathBuf::from("out");
+
+        let should_expand_ce_variables = true;
+        let destination = if should_expand_ce_variables {
+            expand_ce_variables(&file_entry.destination).to_string()
+        } else {
+            file_entry.destination.to_string()
+        };
+
+        println!(
+            "Extracting {} ({} B Compression {:?}) to {}",
+            file_entry.cab_filename, file_entry.file_size, file_entry.compression, destination
+        );
+
+        output_filepath.push(destination);
+
+        fs::create_dir_all(output_filepath.parent().unwrap())
+            .expect("Failed to create output path.");
+        let mut writer = File::create(output_filepath).unwrap();
+        io::copy(&mut reader, &mut writer).unwrap();
+        filetime::set_file_handle_times(&writer, file_entry.file_time, file_entry.file_time).err();
+    }
+}
+
+fn expand_ce_variables(path: &str) -> Cow<str> {
+    CE_DIRS
+        .iter()
+        .fold(Cow::from(path), |s, (from, to)| s.replace(from, to).into())
+}
+
+pub struct WinCECabFileEntry {
+    cab_filename: String,
+    compression: CompressionType,
+    file_time: Option<FileTime>,
+    file_size: u32,
+    destination: String,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,11 @@
-use std::{fs::File, path::Path, io::{Read, Seek}};
-
-use cab::{Cabinet, FileEntry};
-use cabextract_ce::read_msce000;
+use cabextract_ce::{export_files, list_files, read_cab, CECabinet};
 
 fn main() {
-    let cabinet = read_cab("PocketEQDemoPPC2002.STRONGARM.cab");
-    let msce_file_entry = cabinet.get_000_file_entry().expect("Failed to locate .000 file.");
+    let mut cabinet = read_cab("PocketEQDemoPPC2002.STRONGARM.cab");
 
-    println!("Reading {}", msce_file_entry.name());
-    let msce = read_msce000(msce_file_entry.name()).unwrap();
+    let ce_manifest = cabinet.read_000_manifest();
+    let file_entries = list_files(&mut cabinet, &ce_manifest);
+    export_files(&mut cabinet, &file_entries);
 
-    for folder in cabinet.folder_entries() {
-        for file in folder.file_entries() {
-            if file.name() == msce_file_entry.name() {
-                continue;
-            }
-
-            let file_ext = Path::new(file.name()).extension().unwrap().to_string_lossy().to_string();
-            let file_path =  match msce.file_mapping.get(&file_ext) {
-                Some(path) => path,
-                _ => "None",
-            };
-
-            println!("File {} ({} B) {:?} (compression {:?}) destination {}",
-                     file.name(),
-                     file.uncompressed_size(),
-                     file.datetime(),
-                     folder.compression_type(),
-                     file_path);
-        }
-    }
-
-    println!("Done extracting {}", msce.app_name);
-}
-
-fn read_cab(name: &str) -> Cabinet<File> {
-    let cab_file = File::open(name).unwrap();
-    Cabinet::new(cab_file).unwrap()
-}
-
-trait Cabinet000 {
-    fn get_000_file_entry(&self) -> Option<&FileEntry>;
-}
-
-impl<R: Read + Seek> Cabinet000 for Cabinet<R> {
-    fn get_000_file_entry(&self) -> Option<&FileEntry> {
-        for folder in self.folder_entries() {
-            for file in folder.file_entries() {
-                if Path::new(file.name()).extension().unwrap().to_string_lossy().to_string() == "000" {
-                    return Some(file);
-                }
-            }
-        }
-        None
-    }
+    println!("Done extracting {}", ce_manifest.app_name);
 }
